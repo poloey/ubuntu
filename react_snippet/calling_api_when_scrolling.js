6dd641fb2590af436172d9d2c9c0b034f969c4a8
Polo dev <polodev10@gmail.com>
2016-05-07 18:43:40 +0530

    added react snippet for making api call when scroll

---
+++ react_snippet/calling_api_when_scrolling.js
@@ -0,0 +1,22 @@
+var Campaign = React.createClass({
+  componentDidMount: function () {
+    window.addEventListener('scroll', this.handleScroll)
+  },
+
+  handleScroll:function (e) {
+    // var totalScrolled =
+    // $(window).scrollTop() + $(window).innerHeight
+    // if (totalScrolled + 1 > $(window).height()) {
+    //   this.loadMoreSubCampaignsWhileScrolling()
+    // }
+    console.log("window height", $(window).height());
+    console.log("scrollTop", $(window).scrollTop());
+
+    var totalScrolled = $(window).height() + $(window).scrollTop();
+    var documentHeight =   $(document).height() - 1 ; //this 1 is kind of important
+    console.log("totalScrolled", totalScrolled);
+    console.log("documentHeight", documentHeight);
+    if(totalScrolled > documentHeight) {
+        //make api call or do other stuff
+      //this.props.campaignsActions.getNewSubCampaigns();
+    }
