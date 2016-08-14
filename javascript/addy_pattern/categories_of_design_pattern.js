5d755805d83e55e16367dc16468df8a05b466765
Polo dev <polodev10@gmail.com>
2016-08-14 21:16:40 +0530

    added addy osmany design patteren classification and tasksel lamp

---
+++ javascript/addy_pattern/categories_of_design_pattern.js
@@ -0,0 +1,10 @@
+1. Creational Design Patterns
+Abstract, Builder, Constructor, Factory, Prototype, SingleTon
+
+2. Structural Design Patterns
+Adapter, Decorator, Facade, Flyweight, Proxy
+
+3. Behavioral Design Patterns
+Iterator, Mediator, Observer and Visitor
+
+
